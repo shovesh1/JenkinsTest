da4c573220e85b68edde62b876ecc3a75de5b21c
shovesh1 <114637218+shovesh1@users.noreply.github.com>
2022-10-09 12:44:59 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello students from batch27. This is Shovesh"); 
+        System.out.println("Hello students from batch27. This is Shovesh. I have set setup Poll SCM"); 
     }
 }
